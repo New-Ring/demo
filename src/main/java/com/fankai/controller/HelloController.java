8c862ea046c52d37e1a57a72997425b64832a0b2
New-Ring <810647836@qq.com>
2021-04-16 08:46:21 +0800

    测试提交

--- src/main/java/com/fankai/controller/HelloController.java
+++ src/main/java/com/fankai/controller/HelloController.java
@@ -1,9 +1,12 @@
 package com.fankai.controller;
 
+import com.fankai.util.IpAddressUtil;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/say")
@@ -19,5 +22,10 @@ public class HelloController {
         model.addAttribute("loginName", "admin");
         model.addAttribute("loginId", "27");
         return "success";
+    }
+
+    @RequestMapping("/getRemortIP")
+    public String getRemortIP(HttpServletRequest request){
+        return IpAddressUtil.getIP(request);
     }
 }
