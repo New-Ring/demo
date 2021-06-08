b21bb96b1587d2933ac2bab874c5c0864878821a
fankai <810647836@qq.com>
2021-06-08 23:07:01 +0800

    BPMjar

--- src/main/java/com/fankai/controller/HelloController.java
+++ src/main/java/com/fankai/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.fankai.controller;
 
+import com.actionsoft.bpms.commons.database.RowMap;
+import com.actionsoft.bpms.util.DBSql;
 import com.fankai.util.IpAddressUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.net.URLDecoder;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/say")
+@Slf4j
 public class HelloController {
 
     @GetMapping("/hello")
@@ -68,10 +75,22 @@ public class HelloController {
         }
     }
 
-    @RequestMapping("/getRemortIP")
+    @RequestMapping("/updateBpm")
     public void updateBpm(HttpServletRequest request){
-        // update BO_EU_BYY_JXLC_MX set DYKHRPF=25 ,ZPF=25 where id='1e9ff99c-cbab-4c6a-9fe7-37382ab531fa'
-        // update BO_EU_BYY_JXLC_MX set DYKHRPF=:DYKHRPF WHERE ID=:ID
-//        DBSql.update("UPDATE BO_EU_BYY_JXLC_MX set DYKHRPF=25 ,ZPF=25 WHERE ID=? ", "1e9ff99c-cbab-4c6a-9fe7-37382ab531fa");
+        StringBuffer sql = new StringBuffer();
+        sql.append("select * from BO_EU_BYY_JXLC_MX  where id=? " );
+        List<RowMap> pnoList = DBSql.getMaps(sql.toString(), new Object[]{"1e9ff99c-cbab-4c6a-9fe7-37382ab531fa"});
+        if (pnoList.size()>0) {
+            RowMap rowMap = pnoList.get(0);
+            /*for (Map project : projects) {
+                project.put("GLLC", rowMap.getString("BT"));
+                project.put("GLLCID", rowMap.getString("BINDID"));
+            }*/
+        }
+        /*try {
+            DBSql.update("UPDATE BO_EU_BYY_JXLC_MX set DYKHRPF=25 ,ZPF=25 WHERE ID=? ", new Object[]{"1e9ff99c-cbab-4c6a-9fe7-37382ab531fa"});
+        } catch (Exception e) {
+            log.error("考核人评分更新错误：" + e.getMessage());
+        }*/
     }
 }
